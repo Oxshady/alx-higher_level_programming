e027fb2eece86cf33611611e6984fa777a207c45
Oxshady <shadymahmod55@gmail.com>
2024-03-12 21:02:52 +0200

    Decision making

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,8 @@ elif number > 0:
     elif LD == 0:
         print(f"Last digit of {number:d} is {LD:d} and is 0")
     elif LD < 6 and LD != 0:
-        print(f"Last digit of {number:d} is {LD:d} and is less than 6 and not 0")
+        quote = "and is less than 6 and not 0"
+        print(f"Last digit of {number:d} is {LD:d} and {quote}")
 elif number < 0:
     LD = abs(number) % 10
     LD *= -1
@@ -19,4 +20,5 @@ elif number < 0:
     elif LD == 0:
         print(f"Last digit of {number:d} is {LD:d} and is 0")
     elif LD < 6 and LD != 0:
-        print(f"Last digit of {number:d} is {LD:d} and is less than 6 and not 0")
+        quote = "and is less than 6 and not 0"
+        print(f"Last digit of {number:d} is {LD:d} and is {quote}")
