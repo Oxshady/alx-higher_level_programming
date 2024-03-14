de3fe75ce93aa796140938763683e0c7495331b0
Oxshady <shadymahmod55@gmail.com>
2024-03-14 21:38:27 +0200

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1:
-        print("{}".format("0 arguments."), end="")
+        print("{}".format("0 arguments."))
     else:
         if len(argv) == 2:
             print("{} argument:".format(len(argv) - 1))
