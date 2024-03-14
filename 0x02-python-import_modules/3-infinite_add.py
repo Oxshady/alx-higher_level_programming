9e89a0cb97262d2ff9c3c079a740ca6267976739
Oxshady <shadymahmod55@gmail.com>
2024-03-14 21:57:45 +0200

    done

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("{:d}".format(0))
+    else:
+        sum = 0
+        counter = 0
+        for number in argv:
+            counter += 1
+            if counter == 1:
+                continue
+            sum += int(argv[counter - 1])
+    print("{:d}".format(sum))
