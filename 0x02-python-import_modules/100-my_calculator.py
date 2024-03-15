eeb7cf5905e9c6b985b4f62a975e40ae8d8779f9
Oxshady <shadymahmod55@gmail.com>
2024-03-15 04:03:18 +0200

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,3 +14,4 @@ if __name__ == "__main__":
         print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
     elif argv[2] == "-":
         print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+    
