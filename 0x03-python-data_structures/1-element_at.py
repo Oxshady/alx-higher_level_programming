f8f1f8e1a64aba0319ee154c3dc937ccc063c750
Oxshady <shadymahmod55@gmail.com>
2024-03-16 22:21:03 +0200

    done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lenght = len(my_list)
-    if lenght < idx or lenght <= 0:
+    if lenght < idx or lenght < 0:
         return None
     else:
         return my_list[idx]
