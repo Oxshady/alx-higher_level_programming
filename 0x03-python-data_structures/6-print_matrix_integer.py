bad644618354cb03f19d4d8757bb7ba80aea8f0d
Oxshady <shadymahmod55@gmail.com>
2024-03-16 23:41:33 +0200

    done

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+def print_matrix_integer(matrix=[[]]):
+    x = 0
+    for i in matrix:
+        for ii in i:
+            if len(i) - 1 != x: 
+                print("{:d}".format(ii),end=" ")
+            else:
+                print("{:d}".format(ii),end="")
+            x += 1
+        print("{}".format("\n"), end="")
+        x = 0
