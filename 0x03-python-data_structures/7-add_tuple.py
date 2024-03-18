3ff01c6c8cfdbfc0574a3498be60a56f215de50d
Oxshady <shadymahmod55@gmail.com>
2024-03-18 17:25:06 +0200

    done

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,18 +2,24 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     lenght1 = len(tuple_a)
     lenght2 = len(tuple_b)
-    if lenght1 >= 2 and lenght2 >= 2:
-        new = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return new
-    elif lenght1 < 2 and lenght2 >= 2:
-        if lenght1 == 0:
-            new = (0 + tuple_b[0], 0 + tuple_b[1])
-        else:
-            new = (tuple_a[0] + tuple_b[0], 0 + tuple_b[0])
-        return new
-    elif lenght2 < 2 and lenght1 >= 2:
-        if lenght2 == 0:
-            new = (tuple_a[0] + 0, tuple_a[1] + 0)
-        else:
-            new = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-        return new
+
+    if lenght1 >= 2:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+    elif lenght1 == 1:
+        a1 = tuple_a[0]
+        a2 = 0
+    elif lenght1 == 0:
+        a1 = 0
+        a2 = 0
+    if lenght2 >= 2:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+    elif lenght2 == 1:
+        b1 = tuple_b[0]
+        b2 = 0
+    elif lenght2 == 0:
+        b1 = 0
+        b2 = 0
+    new = (a1 + b1, a2 + b2)
+    return (new)
