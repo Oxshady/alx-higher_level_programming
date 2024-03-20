3a53c20fd38113461c8430a606be91797398873f
Oxshady <shadymahmod55@gmail.com>
2024-03-20 23:22:14 +0200

    done

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,7 +2,7 @@
 def only_diff_elements(set_1, set_2):
     res = set({})
     stat = 0
-    for i in set_1:
+    for i in  set_1:
         for ii in set_2:
             if i != ii:
                 stat = 1
@@ -10,8 +10,8 @@ def only_diff_elements(set_1, set_2):
                 stat = 0
         if stat == 1:
             res.add(i)
-    for i in set_2:
-        for ii in set_1:
+    for i in  set_2:
+        for ii in set_2:
             if i != ii:
                 stat = 1
             else:
