e56d0df6d2a7cb86281a272c206ed2616611118f
Oxshady <shadymahmod55@gmail.com>
2024-03-20 23:30:14 +0200

    done

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    res = set({})
-    stat = 0
-    for i in  set_1:
+    res = set()
+    for i in set_1:
+        found = False
         for ii in set_2:
-            if i != ii:
-                stat = 1
-            else:
-                stat = 0
-        if stat == 1:
+            if i == ii:
+                found = True
+                break
+        if not found:
             res.add(i)
-    for i in  set_2:
-        for ii in set_2:
-            if i != ii:
-                stat = 1
-            else:
-                stat = 0
-        if stat == 1:
+    for i in set_2:
+        found = False
+        for ii in set_1:
+            if i == ii:
+                found = True
+                break
+        if not found:
             res.add(i)
     return (res)
