e5a8f5876ca622570f5c17c59579c9b3ff1afb29
Oxshady <shadymahmod55@gmail.com>
2024-04-02 20:36:52 +0200

    discovering more exception types

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             ele += 1
         except IndexError:
-            break
+            raise IndexError("list index out of range")
         except (ValueError, TypeError):
             continue
     print()
-    return(ele)
+    return (ele)
