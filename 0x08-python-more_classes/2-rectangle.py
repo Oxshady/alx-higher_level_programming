6d9944c327a84e07f899b223d3705938cacb2c08
Oxshady <shadymahmod55@gmail.com>
2024-04-09 01:36:06 +0200

    playing with oop

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This module contains the Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class:
+        Private instance attribute: width:
+            property def width(self): to retrieve it
+            property setter def width(self, value): to set it:
+            width must be an integer, otherwise raise a TypeError
+            exception with the message width must be an integer
+            if width is less than 0, raise a ValueError
+            exception with the message width must be >= 0
+        Private instance attribute: height:
+            property def height(self): to retrieve it
+            property setter def height(self, value): to set it:
+            height must be an integer, otherwise raise a
+            TypeError exception with the message height must be an integer
+            if height is less than 0, raise a ValueError
+            exception with the message height must be >= 0
+        Instantiation with optional width and height:
+        def __init__(self, width=0, height=0):
+    """
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return (2 * (self.__width + self.__height))
