3d8a1cf4328b63f7f03ad252a7da6531505a4135
Oxshady <shadymahmod55@gmail.com>
2024-04-09 01:06:21 +0200

    playing with oop

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
 """
-these module contain rectangle class
+This module contains the Rectangle class.
 """
 
 
 class Rectangle:
     """
-    rectangle class:
+    Rectangle class:
         Private instance attribute: width:
-		property def width(self): to retrieve it
-		property setter def width(self, value): to set it:
-		width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-		if width is less than 0, raise a ValueError exception with the message width must be >= 0
-		Private instance attribute: height:
-		property def height(self): to retrieve it
-		property setter def height(self, value): to set it:
-		height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-		if height is less than 0, raise a ValueError exception with the message height must be >= 0
-		Instantiation with optional width and height: def __init__(self, width=0, height=0):
+            property def width(self): to retrieve it
+            property setter def width(self, value): to set it:
+            width must be an integer, otherwise raise a TypeError
+            exception with the message width must be an integer
+            if width is less than 0, raise a ValueError
+            exception with the message width must be >= 0
+        Private instance attribute: height:
+            property def height(self): to retrieve it
+            property setter def height(self, value): to set it:
+            height must be an integer, otherwise raise a
+            TypeError exception with the message height must be an integer
+            if height is less than 0, raise a ValueError
+            exception with the message height must be >= 0
+        Instantiation with optional width and height:
+        def __init__(self, width=0, height=0):
     """
-    def __init__(self, width = 0, height = 0) -> None:
+    def __init__(self, width=0, height=0) -> None:
         self.__width = width
         self.__height = height
-    @property
-    def width(self):
-        return (self.___width)
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+
     @property
     def height(self):
-        return (self.__height)
+        return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -42,3 +39,15 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
