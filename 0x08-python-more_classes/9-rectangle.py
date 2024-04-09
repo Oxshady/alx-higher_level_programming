4c9af8f1afbdb6fe9f9c56205ff5b87ab48fc72e
Oxshady <shadymahmod55@gmail.com>
2024-04-09 03:42:44 +0200

    done

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -99,4 +99,8 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return (cls(size, size))
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        return cls(size, size)
