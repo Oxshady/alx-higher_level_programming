de33aa85942895f9591f8d303c140fe52540020c
Oxshady <shadymahmod55@gmail.com>
2024-04-09 06:54:14 +0200

    doctest is great

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+this module contain function that
+-
+print hi my name is <first name> <last name>
+"""
+
 def say_my_name(first_name, last_name=""):
+    """
+    function that prints hi my name is <first name> <last name>
+
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
