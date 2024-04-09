fd1a67fe61b2713074c847de2f1d77410ed5bdd9
Oxshady <shadymahmod55@gmail.com>
2024-04-09 08:55:34 +0200

    updating

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,8 @@ module that contain -
 function that prints a text with 2 new lines after
 each of these characters: ., ? and :
 """
+
+
 def text_indentation(text):
     """
     Print the text with 2 new lines after each '.', '?', and ':'
@@ -24,14 +26,3 @@ def text_indentation(text):
             start_of_line = True
         else:
             start_of_line = False
-
-if __name__ == "__main__":
-    text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
