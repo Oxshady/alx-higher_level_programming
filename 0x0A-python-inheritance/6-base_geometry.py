e3e8836961a7f244dc828bd0d406dc492f3cdc1a
Oxshady <shadymahmod55@gmail.com>
2024-04-15 22:55:05 +0200

    done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module contain class BaseGeometry that contain 
+Public instance method: def area(self):
+that raises an Exception with the message area() is not implemented
+"""
+
+
+class BaseGeometry:
+    """
+    contain class BaseGeometry that contain 
+    Public instance method: def area(self):
+    that raises an Exception with the message area() is not implemented    """
+    def area(self):
+        """
+        raise exception
+        """
+        raise Exception("area() is not implemented")
