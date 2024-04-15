f42b2b2eddbbe16c49e602ef7a40700902ef4bf9
Oxshady <shadymahmod55@gmail.com>
2024-04-15 21:35:21 +0200

    done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this module contain:
+class MyList that inherits from list
+"""
+class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+    def print_sorted(self):
+        new = sorted(self)
+        print(new)
+        del(new)
