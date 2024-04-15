9695ddade08d51b0bc06d4e9f9a2cc51cc266862
Oxshady <shadymahmod55@gmail.com>
2024-04-15 23:47:58 +0200

    dome

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -19,5 +19,9 @@ class Square(Rectangle):
         self.integer_validator("Size", size)
         super().__init__(size, size)
 
-    def area(self):
-        return self.__size ** 2
+    def __str__(self):
+        """
+        returns a string representation of the square.
+        """
+        s = f"[Rectangle] {self._Rectangle__width}/{self._Rectangle__height}"
+        return (s)
