9b03e824a6cbdf78a489b64be85de4386f34909e
Oxshady <shadymahmod55@gmail.com>
2024-04-15 23:52:19 +0200

    done

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,11 +9,20 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+    class rect
+    """
     def __init__(self, size):
+        """
+        constructor
+        """
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """
+        calc area
+        """
         return self.__size ** 2
 
     def __str__(self):
