3a26855d1fe5ac2a6166fef76512d382aabfd8aa
Oxshady <shadymahmod55@gmail.com>
2024-04-16 14:26:38 +0200

    done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module contain:
+function that read a text file
+"""
+
+
+def read_file(filename=""):
+    """
+    function that read a text file
+    """
+    with open(filename, 'r', encoding="UTF-8") as file:
+        print(file.read())
