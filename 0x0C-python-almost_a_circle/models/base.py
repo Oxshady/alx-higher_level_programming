bd0167cbaeb3ed5071f1f77161b8f8e31c988c55
Oxshady <shadymahmod55@gmail.com>
2024-04-22 18:57:40 +0200

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,29 +24,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
-class TestBase(unittest.TestCase):
-    """
-    class that test base class
-    """
-    def test_id(self):
-        """
-        test class with and without id
-        """
-        base1 = Base(20)
-        base2 = Base()
-        base3 = Base()
-        base4 = Base(-2)
-        base5 = Base(14.1)
-        base6 = Base()
-        base7 = Base(None)
-        self.assertEqual(base1.id, 20)
-        self.assertEqual(base2.id, 1)
-        self.assertEqual(base3.id, 2)
-        self.assertEqual(base4.id, -2)
-        self.assertEqual(base5.id, 14.1)
-        self.assertEqual(base6.id, 3)
-        self.assertEqual(base7.id, 4)
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         returns the JSON string representation of list_dictionaries
