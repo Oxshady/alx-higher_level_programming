0fa0788db6f6ea2f274811e243ceee64d454b026
Oxshady <shadymahmod55@gmail.com>
2024-04-22 19:13:18 +0200

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,6 +33,15 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """
