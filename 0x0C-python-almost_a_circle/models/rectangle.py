cf4dc73ed72776cff28d0d1fcc726a0f568c5c15
Oxshady <shadymahmod55@gmail.com>
2024-04-23 09:20:31 +0200

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -134,6 +134,21 @@ class Rectangle(Base):
         for _ in range(self.height):
             print(" " * self.x + "#" * self.width)
 
+    def update(self, *args):
+        """
+        Update instance of rectangle
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def update(self, **kwargs):
         """
         update instance of rectangle
