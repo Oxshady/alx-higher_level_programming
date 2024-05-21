5895d01bde64cd0f6ff1ee405db3ed9dc23a5d3d
homos <homos@github.com>
2024-05-22 01:11:35 +0300

    donegit

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- select ordering by top scores
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
