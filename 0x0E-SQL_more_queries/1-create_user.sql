8a0b154e1f108e60e4e8d659fe4d9d31a1114de5
homos <homos@github.com>
2024-05-22 21:16:38 +0300

    new user with super priv

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create new user with privillage on mysql server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILLAGE ON "." 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
