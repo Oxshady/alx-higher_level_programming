5a5f69078132d9b54e7ac02bc3a1f98c0f0e1066
Oxshady <shadymahmod55@gmail.com>
2024-06-19 01:03:02 +0300

    done

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const leng = process.argv.length
+
+if (leng < 4) {
+	console.log(0);
+} else {
+	let largest = 0;
+	let largest2 = 0;
+	for (let i = 2; i < leng; i++) {
+		if (largest < Number(process.argv[i])) {
+			largest = Number(process.argv[i]);
+		}
+	}
+	for (let i = 2; i < leng; i++) {
+		if (largest2 < Number(process.argv[i])) {
+			if (Number(process.argv[i]) < largest) {
+				largest2 = Number(process.argv[i]);
+			}
+		}
+	}
+	console.log(largest2)
+}
