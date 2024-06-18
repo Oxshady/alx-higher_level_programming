52b0bb63c4d6521f262e2cfd73ed23321fbf4319
Oxshady <shadymahmod55@gmail.com>
2024-06-18 21:28:50 +0300

    easy task

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 
 const st1 = "C is fun", st2 = "Python is cool", st3 = "JavaScript is amazing"
-console.log(st1 + "\n" + st2 + "\n" + st3)
+console.log(st1)
+console.log(st2)
+console.log(st3)
