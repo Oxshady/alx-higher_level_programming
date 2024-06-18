df1b3059bfc94a766e26c4846b46fa7dc9ae01cb
Oxshady <shadymahmod55@gmail.com>
2024-06-18 23:43:17 +0300

    nan or not to add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 function add(a, b) {
-	if (isNaN(a) || isNaN(b)) {
+	num1 = Number(a);
+	num2 = Number(b);
+	if (isNaN(num1) || isNaN(num2)) {
 		console.log("NaN");
 	} else {
-		console.log(a + b);
+		console.log(num1 + num2);
 	}
 }
 add(process.argv[2], process.argv[3]);
