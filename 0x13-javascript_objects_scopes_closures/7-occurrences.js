197fd3b6d3eace65ab6b4dcf39414d5020e2347f
Oxshady <shadymahmod55@gmail.com>
2024-06-19 12:04:27 -0700

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const i in list) {
+    if (searchElement === list[i]) {
+      count++;
+    }
+  }
+  return count;
+};
