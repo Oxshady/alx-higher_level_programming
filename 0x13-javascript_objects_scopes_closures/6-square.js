971c012488e6417f49ae3e0e5dc4b2802faaa68d
Oxshady <shadymahmod55@gmail.com>
2024-06-19 11:38:46 -0700

    methods

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let sym = ''; let leng = 0;
+    if (c === undefined) {
+      c = 'X';
+    }
+    while (leng < this.width) {
+      sym += c;
+      leng++;
+    }
+    leng = 0;
+    while (leng < this.height) {
+      console.log(sym);
+      leng++;
+    }
+  }
+}
+module.exports = Square;
