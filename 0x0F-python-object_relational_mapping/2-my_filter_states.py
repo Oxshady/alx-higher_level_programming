6e849b642a1e787452a12241c086bc60532d609c
Oxshady <shadymahmod55@gmail.com>
2024-06-28 22:01:53 +0300

    filter using user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,11 @@ db_name = argv[3]
 state = argv[4]
 s = "SELECT * from states WHERE name = %s ORDER BY id ASC"
 try:
-    connection = MySQLdb.connect(host = 'localhost', port = 3306,user=db_user, passwd=db_pass, db=db_name)
+    connection = MySQLdb.connect(host='localhost',
+                                 port=3306,
+                                 user=db_user,
+                                 passwd=db_pass,
+                                 db=db_name)
     with connection:
         cursor = connection.cursor()
         with cursor:
