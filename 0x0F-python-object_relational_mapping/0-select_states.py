5bbe9d224d77414dd7438691ac43a50ebf9363f2
Oxshady <shadymahmod55@gmail.com>
2024-06-28 04:07:54 +0300

    Lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,8 +4,15 @@ Lists all states from the database hbtn_0e_0_usa
 Results must be sorted in ascending order by states.id
 """
 import MySQLdb
+from sys import argv
+if len(argv) < 4:
+    print("3 argument required")
+    exit()
+db_user = argv[1]
+db_pass = argv[2]
+db_name = argv[3]
 try:
-    connection = MySQLdb.connect(user="shadi", passwd="1", db="hbtn_0e_0_usa")
+    connection = MySQLdb.connect(user=db_user, passwd=db_pass, db=db_name)
     with connection:
         cursor = connection.cursor()
         with cursor:
