e7bf0b742015495a700bb12175233e38adae8c53
Oxshady <shadymahmod55@gmail.com>
2024-07-10 07:02:21 +0300

    body only if 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#get the length of the response
+#get body if res = 200
 curl -sI "$1" -o response.txt && code=$(grep "HTTP" response.txt | cut -d ' ' -f2) && if [ "$code" -eq 200 ]; then curl -s "$1";fi && rm -f response.txt
