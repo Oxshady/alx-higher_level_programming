221e28dec94e78461200151870d3cc1e8af7ca85
Oxshady <shadymahmod55@gmail.com>
2024-07-10 07:19:24 +0300

    allow header value

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #all av methods
-curl -sI -X OPTIONS "$1" -o res.txt && grep -i "Allow:" res.txt | cut -d ":" -f2-
+curl -sI -X OPTIONS "$1" -o res.txt && grep -i "Allow:" res.txt | cut -d ":" -f2- | cut -d ' ' -f2- && rm -f res.txt
