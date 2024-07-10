807df6e1b4dc9da4ec7604958b3360d56777d2ac
Oxshady <shadymahmod55@gmail.com>
2024-07-10 05:45:54 +0300

    lenght of resp body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,6 +3,6 @@
 if [ $# -lt 1 ]; then
 	exit 1
 fi
-curl -sI $1 -o response.txt
-cat response.txt | grep "Content-Length" | cut -d ':' -f2 | cut -d " " -f2
+curl -sI "$1" -o response.txt
+grep "Content-Length" response.txt | cut -d ':' -f2 | cut -d ' ' -f2
 rm -f response.txt
