8fdb8570ed9bae30fc59a1c6d9acb6c440ecf799
Oxshady <shadymahmod55@gmail.com>
2024-07-11 02:23:43 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+ Python script that fetches
+ https://alx-intranet.hbtn.io/status
+"""
+
+import urllib
+import urllib.request
+
+req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen(req) as res:
+    ress = res.read()
+    x = f"Body response:\n\t- type: {type(ress)}\n\t"
+    print(x + f"- content: {ress}\n\t- utf content: {ress.decode()}")
