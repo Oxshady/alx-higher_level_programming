978147d9746eb2ed5ad980675db04704f8e70aef
Oxshady <shadymahmod55@gmail.com>
2024-07-11 05:08:17 +0300

    handle responses to get only the succesfull ones

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays the body of the response
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.get(url=url)
+    if res.ok:
+        print(res.text)
+    else:
+        print(f"Error code: {res.status_code}")
