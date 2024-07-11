710617e4fd0358526868a07378ae2765bd2d96ad
Oxshady <shadymahmod55@gmail.com>
2024-07-11 05:21:59 +0300

    json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+ script that takes in a letter and sends a POST request
+ to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    value = sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+    q = {"q": ""}
+    if value:
+        q = {"q": value}
+    res = requests.post(url=url, data=q)
+    if "application/json" in res.headers.get('Content-Type'):
+        res = res.json
+        if res:
+            print(f"[{res['id']}] {res['name']}")
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
