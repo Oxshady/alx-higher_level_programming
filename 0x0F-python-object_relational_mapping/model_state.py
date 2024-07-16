ba1ca8b32c423332ab8cef4912146c4f9a81521e
Oxshady <shadymahmod55@gmail.com>
2024-07-17 02:08:15 +0300

    ORM no sql more

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
+
 """
 implement object relation mapping using sqlalchemy
 mapping class state to states table in relational db
 """
 
 
-from sqlalchemy import create_engine, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker, mapped_column
-from sqlalchemy.orm import Mapped
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import declarative_base, mapped_column, Mapped
 from sys import argv
 
 
-url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
-engine = create_engine(url)
 Base = declarative_base()
-Session = sessionmaker(bind=engine)
 
 
 class State(Base):
@@ -23,10 +20,6 @@ class State(Base):
         id (int): id of state
         name (string): name of state
     """
-    tablename__ = "states"
+    __tablename__ = "states"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    Base.metadata.create_all(engine)
