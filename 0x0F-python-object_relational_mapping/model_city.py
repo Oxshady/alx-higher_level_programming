3fd98fa7487fb997079c03ca2414e81bca0f4a8a
Oxshady <shadymahmod55@gmail.com>
2024-07-17 06:44:07 +0300

    city implementation and est rel with state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+implement object relation mapping using sqlalchemy
+mapping class city to  cities table in relational db
+"""
+
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import declarative_base, mapped_column, Mapped
+from sys import argv
+
+
+Base = declarative_base()
+
+
+class City (Base):
+    """City represent table in relational db
+    Args:
+        id (int): id of city
+        name (string): name of city
+        state_id (int): id of state to est rel
+    """
+    __tablename__ = "cities"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    state_id: Mapped[int] = mapped_column(ForeignKey('states.id'))
