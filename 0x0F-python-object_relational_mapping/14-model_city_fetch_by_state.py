aee8ff8a77373822431345f6bb760a3b5f9b15b0
Oxshady <shadymahmod55@gmail.com>
2024-07-17 06:41:21 +0300

    relationship implementation

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+implement object relation mapping using sqlalchemy
+delete object
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        data = session.query(City, State)
+        data = data.filter(City.state_id == State.id).all()
+        for city, state in data:
+            print(f"{state.name}: ({city.id}) {city.name}")
