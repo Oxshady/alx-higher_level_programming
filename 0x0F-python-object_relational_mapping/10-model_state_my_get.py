d37e9eebf499e5a27fb7b6e1a9b688afcb5289f0
Oxshady <shadymahmod55@gmail.com>
2024-07-17 04:46:16 +0300

    filtering and do db functions

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+implement object relation mapping using sqlalchemy
+get all rows that contain a character
+"""
+
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+state_name = argv[4]
+
+if __name__ == "__main__":
+    url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        x = session.query(func.count(State.id))
+        x = x.filter_by(name=state_name).first()
+        print(x[0])
