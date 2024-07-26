4f1e46c8bacd73f973816a59f5684167e50c8ffb
Oxshady <shadymahmod55@gmail.com>
2024-07-27 01:20:42 +0300

    establish one to may relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+import relationship_city
+import relationship_state
+if __name__ == "__main__":
+    base = relationship_city.Base
+    State = relationship_state.State
+    City = relationship_city.City
+    url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    base.metadata.create_all(bind=engine)
+    state1 = State(name="California")
+    city = City(name="San Francisco", state=state1)
+    with Session() as session:
+        try:
+            session.add(city)
+            session.commit()
+        except Exception:
+            session.rollback()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+implement object relation mapping using sqlalchemy
+mapping class state to states table in relational db
+"""
+
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import (declarative_base, mapped_column,
+                            Mapped, relationship)
+from sys import argv
+import relationship_city
+from typing import List
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State represent table in relational db
+    Args:
+        id (int): id of state
+        name (string): name of state
+    """
+    __tablename__ = "states"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    cities: Mapped[List["relationship_city.City"]] = relationship(
+        backref="state", cascade="all, delete-orphan"
+        )
