56966fbb10b19e96474c54335b1be32ae199f910
Oxshady <shadymahmod55@gmail.com>
2024-07-27 02:30:14 +0300

    join is done

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -8,14 +8,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sys import argv
 
-url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
-engine = create_engine(url)
-Session = sessionmaker(bind=engine)
-with Session() as session:
-    State = relationship_state.State
-    City = relationship_city.City
-    data = session.query(State).join(City).order_by(State.id, City.id).all()
-    for state in data:
-        print(f"{state.id}: {state.name}")
-        for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
+if __name__ == "__main__":
+    url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        State = relationship_state.State
+        City = relationship_city.City
+        data = session.query(State).join(City).order_by(State.id, City.id).all()
+        for state in data:
+            print(f"{state.id}: {state.name}")
+            for city in state.cities:
+                print(f"\t{city.id}: {city.name}")
