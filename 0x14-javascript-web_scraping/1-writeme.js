1817e87ae2fb31c479f53a96555fe20aded0eaba
Oxshady <shadymahmod55@gmail.com>
2024-07-30 20:46:46 +0300

    writing to file in async

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (arg) {
-  const fs = require('fs');
-  fs.readFile(arg, { encoding: 'utf-8' }, (err, data) => {
+const fs = require('fs');
+const fname = process.argv[2];
+const data = process.argv[3];
+
+if (fname && data) {
+  fs.writeFile(fname, data, { encoding: 'utf-8' }, (err) => {
     if (err) {
       console.log(err);
-    } else {
-      console.log(data);
     }
   });
 }
